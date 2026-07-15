92557a1e637e3e72494ad2d64a43749226f03a0d
Mohd Rumman Khan <mohdrummankhan96@gmail.com>
2026-07-15 20:31:16 +0530

    only start if Database is up

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const {userRouter} = require("./routes/user");
 const {courseRouter} = require("./routes/course");
 const {adminRouter} = require("./routes/admin");
+const mongoose =require('mongoose');
+
 
 
 app.use("/api/v1/user" , userRouter);
@@ -10,8 +12,10 @@ app.use("/api/v1/admin" , adminRouter);
 app.use("/api/v1/course" , courseRouter);
 
 
- 
-app.listen(3000);
+async function main(){
+    await mongoose.connect("mongodb+srv://dbUser1:mre5rO92tDPxhXRh@cluster0.hmbza7e.mongodb.net/Course-App");
+    app.listen(3000);
+    console.log("Server is running on port 3000");
+}
 
-
-
+main()
